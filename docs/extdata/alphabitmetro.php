0c48fd33b2b33a48a878cde1ed6b5328d98b3468
Skif <skif@ubuntu-mob.swarga.local>
2015-08-15 19:53:08 +0300

    удаление неплательщиков Щукинская Щёлково, Красногорск, Митино, Цветной Бульвар, Марьина роща

--- docs/extdata/alphabitmetro.php
+++ docs/extdata/alphabitmetro.php
@@ -17,8 +17,8 @@
 			<p><img src="/pics/line3.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/5.html" title="Штампы и печати за час на Кунцевской" >м. Кунцевская</a></p>
 		</td>
 		<td width="34%">
-			<p><img src="/pics/line10.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/46.html" title="" >м. Марьина роща</a></p>
-			<p><img src="/pics/line3.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/50.html" title="Изготовление факсимиле в Митино" >м. Митино</a></p>			
+			<!--<p><img src="/pics/line10.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/46.html" title="" >м. Марьина роща</a></p>-->
+			<!--<p><img src="/pics/line3.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/50.html" title="Изготовление факсимиле в Митино" >м. Митино</a></p>-->			
 			<p><img src="/pics/line3.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/51.html" title="Изготовление факсимиле на Молодёжной" >м. Молодёжная</a></p>
 			<p><img src="/pics/line8.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/37.html" title="Сделать печать в Новогиреево" >м. Новогиреево</a></p>
 			<p><img src="/pics/line6.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/10.html" title="Изготовление печатей по оттиску в Новых Черемушках" >м. Новые черемушки</a></p>
@@ -30,17 +30,18 @@
 			<p><img src="/pics/line9.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/27.html" title="Изготовить печать на Савеловской" >м. Савеловская</a></p>
 			<p><img src="/pics/line7.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/13.html" title="Изготовление гербовых печатей на Таганке" >м. Таганская</a></p>
 			<p><img src="/pics/line9.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/33.html" title="Печати с защитой на Тульской"  >м. Тульская</a></p>
-		</td>
-		<td width="33%">		 		    	
-			<p><img src="/pics/line9.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/9.html" title="Многоцветные печати на Цветном бульваре" >м. Цветной бульвар</a></p>
-			<p><img src="/pics/line3.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/29.html" title="Изготовление печатей по оттиску на Щелковской" >м. Щелковская</a></p>
-			<p><img src="/pics/line7.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/42.html" title="Срочное Изготовление печатей на Щукинской" >м. Щукинская</a></p>
+			<!--<p><img src="/pics/line9.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/9.html" title="Многоцветные печати на Цветном бульваре" >м. Цветной бульвар</a></p>-->
+			<!--<p><img src="/pics/line3.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/29.html" title="Изготовление печатей по оттиску на Щелковской" >м. Щелковская</a></p>
+			<p><img src="/pics/line7.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/42.html" title="Срочное Изготовление печатей на Щукинской" >м. Щукинская</a></p>-->
 			<p><img src="/pics/line3.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/14.html" title="Срочное изготовление печатей на Электрозаводской" >м. Электрозаводская</a></p>
 			<p><img src="/pics/line1.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/15.html" title="Сделать печать или штамп на Юго-Западной">м. Юго-Западная</a></p>
+		</td>
+		<td width="33%">		 		    	
+			
 			<p><img src="/pics/line9.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/24.html" title="Изготовление печатей по оттиску на Южной" >м. Южная</a></p>
 		<!--	<p><img src="/pics/line6.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/35.html">м. Ясенево</a></p>   -->
 		    <p><a href="/contacts/20.html" title="Срочное изготовление печатей и штампов в Зеленограде" >Зеленоград</a></p>
-			<p><a href="/contacts/17.html" title="Сделать печать в Красногорске" >Красногорск</a></p>
+			<!--<p><a href="/contacts/17.html" title="Сделать печать в Красногорске" >Красногорск</a></p>-->
 			<p><a href="/contacts/18.html" title="Печати по оттиску в Одинцово"  >Одинцово</a></p>
 			<p><a href="/contacts/40.html" title="Печати и штампы по оттиску в Солнцево" >Солнцево</a></p>
 			<p><a href="/contacts/47.html" title="Изготовление печатей в Румянцево" >Румянцево</a></p>
