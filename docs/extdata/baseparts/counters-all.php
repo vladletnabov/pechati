b78bc1b886c78f3fc5e721ea4cf3d008d6eb7b3f
vladletnabov <v.chigikov@yandex.ru>
2015-09-01 10:16:43 +0300

    31.08.2015
    Коммит вчерашнего дня

---
+++ docs/extdata/baseparts/counters-all.php
@@ -0,0 +1,40 @@
+<div><div style="display:none;"><!--550080641835--></div><noindex>
+<!--LiveInternet counter--><script type="text/javascript"><!--
+document.write("<a href='http://www.liveinternet.ru/click' "+
+"target=_blank><img src='http://counter.yadro.ru/hit?t45.1;r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";"+Math.random()+
+"' alt='' title='LiveInternet' "+
+"border=0 width=31 height=31><\/a>")//--></script><!--/LiveInternet-->
+
+<!--- SmartLog ---><script language="javascript">
+i=Math.random();w=screen.width;h=screen.height;
+bps=screen.colorDepth;ln=navigator.systemLanguage;
+if(navigator.cookieEnabled) cook = "Y"; else cook = "N";
+ document.write('<a href="http://www.smartlog.ru/" target="_top"><img width=1 height=1 src="http://www.smartlog.ru/counter/?i='+i+'&cid=999&r='+escape(document.referrer)+'&sid=1193&cook='+cook+'&s='+w+'x'+h+'&pg='+escape(window.location.href)+'" alt="SmartLog.ru - интернет статистика" border=0></a>')
+</script><!--- SmartLog --->
+
+<!--begin of Rambler's Top100 code -->
+<a href="http://top100.rambler.ru/top100/">
+<img src="http://counter.rambler.ru/top100.cnt?146655" alt="" width=1 height=1 border=0></a>
+
+<!--end of Top100 code--><!--begin of Top100 logo-->
+<a href="http://top100.rambler.ru/top100/">
+<img src="http://top100-images.rambler.ru/top100/banner-88x31-rambler-gray2.gif" alt="Rambler's Top100" width=88 height=31 border=0></a>
+<!--end of Top100 logo -->
+
+<!-- Yandex.Metrika counter -->
+<script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript"></script>
+<script type="text/javascript">
+try { var yaCounter114010 = new Ya.Metrika({id:114010,
+          clickmap:true,
+          accurateTrackBounce:true, webvisor:true});}
+catch(e) { }
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/114010" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+</noindex>
+</div>
+
