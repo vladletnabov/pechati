35ae6ab13a2b0731570692e878045feb745fb8ad
Skif <v.chigikov@yandex.ru>
2015-09-01 14:33:23 +0300

    Новые филиалы

---
+++ docs/extdata/filials/bratislavskaya.php
@@ -0,0 +1,83 @@
+<div id="contacts1">
+	<!--<div style="float: right;"><a href="javascript: window.print();">Распечатать</a></div>
+	<p><img src="../i/0.gif" class="date1 date2 poi2" width="13" height="13" alt="" align="absmiddle"> м. Братиславская.</p>-->
+	<table width="495" border="0">
+	  <tr>
+	    <td>&nbsp;</td>
+	    <td colspan="3">Филиал Печати.ru на Братиславской, находится в шаговой доступности от метро, сразу за торговым центром БУМ. <br></td>
+	    <td colspan="2" rowspan="5">пн<br>
+	      вт<br>
+	      ср<br>
+	      чт<br>
+	      пт<br>
+	      <br>
+	      сб<br>
+	      вс<br></td>
+	    <td width="94" rowspan="5">09:00-21:00<br>
+	      09:00-21:00<br>
+	      09:00-21:00<br>
+	      09:00-21:00<br>
+	      09:00-21:00<br>
+	      <br>
+	      09:00-21:00   09:00-21:00</td>
+	    <td width="10">&nbsp;</td>
+	    </tr>
+	  <tr>
+	    <td width="1">&nbsp;</td>
+	    <td width="7">&nbsp;</td>
+	    <td width="6">&nbsp;</td>
+	    <td width="332">&nbsp;</td>
+	    <td>&nbsp;</td>
+	    </tr>
+	  <tr>
+	    <td>&nbsp;</td>
+	    <td colspan="3">г. Москва, ул. Перерва, д. 43, 2-й этаж, оф. А18<br>(ТЦ Экватор)</td>
+	    <td>&nbsp;</td>
+	    </tr>
+	  <tr>
+	    <td>&nbsp;</td>
+	    <td>&nbsp;</td>
+	    <td>&nbsp;</td>
+	    <td>&nbsp;</td>
+	    <td rowspan="2">&nbsp;</td>
+	    </tr>
+	  <tr>
+	    <td height="39">&nbsp;</td>
+	    <td colspan="3">Тел.: <strong>8 (925) 498-20-48</strong><br>
+	      </p></td>
+	    </tr>
+	  <tr>
+	    <td height="47">&nbsp;</td>
+	    <td colspan="3">e-mail: <a href="mailto:marino@pechati.ru">marino@pechati.ru</a><a href="mailto:marino@pechati.ru"></a></td>
+	    <td width="19">&nbsp;</td>
+	    <td width="6">&nbsp;</td>
+	    <td>&nbsp;</td>
+	    <td>&nbsp;</td>
+	    </tr>
+	  </table><br>
+     
+<script type="text/javascript" charset="utf-8" src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=CqGwtqMn7q0JFCPQbMY6fG0U6PAUqsC6&width=485&height=335"></script><br>     
+      
+      
+	<p> - - - - - - - - - - - - - - - - - Как к нам пройти<span style="text-align: center"></span> - - - - - - - - - - - - - - - - - <br>  <br>  
+
+      <strong>1)	ст. м. Братиславская. </strong>Выходите из первого вагона. Первый выход наверх, поворачиваете налево к ТЦ "Бум". Проходите ТЦ "Бум" насквозь. Видите 3 высоких здания. ТЦ "Экватор" находится в правом здании. С правой стороны будут ступеньки наверх. Вам сюда. 
+
+<br>  
+    <img src="/pics/contacts/bratislavskaya/1.jpg" width="450px" 		 />
+<br>  
+<strong>2)	ст.м. Марьино.</strong> Выход последний вагон из центра. Выходите на улицу Люблинская. Следуйте по Люблинской улице до ТЦ "Ашан" (примерно 600 метров). Перейдите улицу Перерва. Идите вдоль дублера улицы Перерва  до дома № 43 (примерно 400 метров). По левой стороне увидите ТЦ "Экватор". С правой стороны будут ступеньки наверх. Вам сюда.
+	<br>  
+		<img src="/pics/contacts/bratislavskaya/2.jpg" width="450px"  />
+<br>  
+
+<strong>3)	ст. м. Люблино.</strong> Выходите в город в сторону улицы Краснодарской. Пройти по ней 840 метро до пересечения с Краснодонской улицей, по которой, в свою очередь, необходимо дойти до улицы Братиславская. По улице Братиславская необходимо  пройти по круговому движению и повернуть направо на улицу Перерва. По улице Перерва пройти 350 метро. По правой стороне увидите ТЦ "БУМ" его необходимо обойти (или прости насквозь). Вы попадаете на дублёр. Идите по нему в сторону ТЦ "Ашан" по правой стороне вы увидите ТЦ "Экватор". Вход в офис располагается с торца здания (ступени на второй этаж). Ориентиром может служить большая вывеска "Школьная форма".
+</p>
+
+    <img src="/pics/contacts/bratislavskaya/3.jpg"	 width="450px" />	
+
+
+
+
+
+</div>
