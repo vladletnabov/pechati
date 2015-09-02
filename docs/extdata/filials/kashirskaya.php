b742a75c5f03e22449660fc0372b4a9f7bc1a9b1
Skif <v.chigikov@yandex.ru>
2015-09-02 15:28:45 +0300

    do 20.html

---
+++ docs/extdata/filials/kashirskaya.php
@@ -0,0 +1,19 @@
+<div id="contacts1">
+	<!--<div style="float: right;"><a href="javascript: window.print();">Распечатать</a></div>
+	<p><strong>Схема проезда до офиса компании "Печати.ру" на Каширской</strong></p>
+	<p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle"> м. Каширская<br>-->
+	<p>Каширское шоссе, д.26, кор.3, вход со двора<br>
+	Тел. 8 (495) 728-45-98, (985) 915-46-85, (903) 527-39-47.</p>
+	<p>Часы работы: пн-чт 10:00-19:00, пт 10:00-18:00, в выходные по предварительной договорённости</p>
+	<p>Для изготовления печатей и штампов: <a href="mailto:pechati16@yandex.ru">pechati16@yandex.ru</a></p>
+	<p><img src="../pics/contacts/kashirka6.jpg" width="495" height="371" alt=""></p>
+	<p><img src="../pics/contacts/kashirka5.jpg" width="495" height="371" alt=""></p>
+	<p><img src="../pics/contacts/kashirka4.jpg" width="495" height="371" alt=""></p>
+	<p align="center"><a href="http://maps.yandex.ru/?um=uEH3cpk5MVMkp2einfyVT1--yita4zUo&l=map" target="_blank">Динамическая карта</a></p>
+	<!--<p><strong>Схема проезда до офиса компании "Печати.ру" на Каширской</strong></p>
+	<p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle"> м. Каширская<br>
+	Тел. 8 (499) 391-97-27 </p> -->
+	<!--<p>Часы работы: пн-чт 10:00-19:00, пт 10:00-18:00, в выходные по предварительной договорённости</p>-->
+	<!--<p>Для изготовления печатей и штампов: <a href="mailto:chertanovo@pechati.ru">chertanovo@pechati.ru</a></p>
+	Офис на м. Каширская в Июле временно не работает. Обращайтесь в офис на м. Пражская или мы бесплатно доставим Вам Ваш заказ</p> -->
+</div>
---
+++ docs/extdata/filials/lubertsy.php
@@ -0,0 +1,7 @@
+<div id="contacts1">
+	<!--<div style="float: right;"><a href="javascript: window.print();">Распечатать</a></div>
+	<p><strong>Схема проезда до офиса компании "Печати.ру" в Люберцах</strong></p>
+	<p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle">М.О., г. Люберцы<br>-->
+	<p>Тел.: <small>(926)</small> 276-61-86<br>район Некрасовка ул.1-ая Вольская, д.18 кор.2, "Дом быта"</p>
+	<p><a href="mailto:festnashmir@mail.ru">festnashmir@mail.ru</a></p>
+</div>
