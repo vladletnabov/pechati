dbd288373fd136ccc324ed00b9799ec2d353173c
vladletnabov <v.chigikov@yandex.ru>
2015-09-04 17:17:43 +0300

    41-49
    обновлены страницы

---
+++ docs/extdata/filials/delovoy-center.php
@@ -0,0 +1,19 @@
+<div id="contacts1">
+	<!--<div style="float: right;"><a href="javascript: window.print();">Распечатать</a></div>-->
+	<p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle"> м. Деловой центр<br>Тел.: 8 <strong>(495)</strong> 741-10-58<br>Пресненская набережная, д. 12</p>
+	<p>Почта: <a href="mailto:mcity@pechati.ru">mcity@pechati.ru</a></p>
+	<p>Часы работы: Пн-Пт с 10:00 до 19:00</p>
+
+	<p>Метро <strong>«Выставочная»</strong>, <strong>«Деловой центр»</strong></p>
+
+	<p>Московский международный деловой центр <strong>«Москва-Сити»</strong> (ММДЦ «Москва-Сити»).</p>
+
+	<p>Башня <strong>«Федерация Восток»</strong></p>
+
+
+	
+	<img src="/pics/delovoy-center/vostok2.jpg" alt="ПечатиРУ у метро Деловой Центр" width="490px" border="1px" Style="margin-bottom:10px;" />
+
+	<img src="/pics/delovoy-center/vostok.jpg" alt="ПечатиРУ на Выставочной" width="490px" border="1px" />
+	
+</div>
---
+++ docs/extdata/filials/dmitrovskaya.php
@@ -0,0 +1,9 @@
+<div id="contacts1">
+	<div style="float: right;"><a href="javascript: window.print();">Распечатать</a></div>
+	<p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle"> м. Дмитровская, Савёловская, Динамо.<br>Тел.: <b>(916)</b> 993-19-19<br>ул. Писцовая, д.16, к. 2</p>
+	<p>Почта: <a href="mailto:pr@pechati.ru">pr@pechati.ru</a></p>
+	<script type="text/javascript" charset="utf-8" src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=cEUkNZyHj6wCw42anmE1GgbHdRHIOp93&width=495&height=450"></script>
+	<p><img src="../pics/contacts/savok1.png" width="495" alt=""></p>
+	<p><img src="../pics/contacts/savok2.png" width="495" alt=""></p>
+	<p><a href="http://maps.yandex.ru/-/CVR-zW6T">Просмотреть увеличенную карту</a></p>
+</div>
