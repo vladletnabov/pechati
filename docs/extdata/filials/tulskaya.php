5dc61adfaa3dd7a90279c722e5865acafb3c877c
Skif <v.chigikov@yandex.ru>
2015-09-07 23:57:31 +0300

    возврат изменений на uslugi/index, замена адреса почты для Тульской

--- docs/extdata/filials/tulskaya.php
+++ docs/extdata/filials/tulskaya.php
@@ -10,6 +10,6 @@
 	<p>Почта: <a href="mailto:t@pechati.ru">t@pechati.ru</a></p> -->
 Тел.: <b>8 (495) 532-92-20</b><br>
 ул. Большая Тульская, д. 13 (ТРЦ Ереван Плаза)</p>
-	<p>Почта: <a href="mailto:t@pechati.ru">t@pechati.ru</a></p>
+	<p>Почта: <a href="mailto:tlk@pechati.ru">tlk@pechati.ru</a></p>
 <br><br><script type="text/javascript" charset="utf-8" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=1TJYlL2e-orwtvqk8Pe-jBYG_apb7nEX&width=495&height=450"></script>
 </div>
