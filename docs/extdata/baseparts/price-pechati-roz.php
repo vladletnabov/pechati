8afb619e6737477147712044c6c77eb1ace327b7
vladletnabov <v.chigikov@yandex.ru>
2015-09-09 14:53:50 +0300

    Перенос цен с розницы в отдельный файл

---
+++ docs/extdata/baseparts/price-pechati-roz.php
@@ -0,0 +1,70 @@
+<script>
+var price1 = 550;
+</script>
+<TABLE border=0 cellSpacing=1 cellPadding=5 width="100%">
+<TR bgcolor="#ebeaef">
+<TD>
+<a name="klishe"></a><!--<P align=center><B>Клише из резины. </B></P>-->
+</TD>
+<TD>
+<P align=center><B>1 день</B></P></TD>
+<TD>
+<P align=center><B>Срочно</B></P></TD></TR>
+<TR bgcolor="#ebeaef">
+<TD><strong>НОВЫЕ</strong> печати или штампы</TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону за 1 день" checked onClick="price1 = 550; count_price();" id="srok3"> <label for="srok3">550</label></P></TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону срочно" onClick="price1 = 850; count_price();" id="srok4"> <label for="srok4">850</label></P></TD></TR>
+<TR bgcolor="#ebeaef">
+<TD>Печати, штампы, факсимиле <strong>по ОТТИСКУ</strong></TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Печать, штамп по оттиску за 1 день" onClick="price1 = 850; count_price();" id="srok1"> <label for="srok1">850</label></P></TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Печать, штамп по оттиску срочно" onClick="price1 = 1400; count_price();" id="srok2"> <label for="srok2">1400</label></P></TD></TR>
+<TR bgcolor="#ebeaef">
+<TD>Факсимиле</TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Факсимиле за 1 день" onClick="price1 = 650; count_price();" id="srok5"> <label for="srok5">650</label></P></TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Факсимиле срочно" onClick="price1 = 950; count_price();" id="srok6"> <label for="srok6">950</label></P></TD></TR>
+<TR bgcolor="#ebeaef">
+<TD>Изготовление <a href="/uslugi/ekslibris.html">экслибриса</a>, <a href="/uslugi/ekslibris.html">личной печати</a></TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Изготовление экслибриса, личной печати за 1 день" onClick="price1 = 2000; count_price();" id="srok13"> <label for="srok13">2000</label></P></TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Изготовление экслибриса, личной печати срочно" onClick="price1 = 5000; count_price();" id="srok14"> <label for="srok14">5000</label></P></TD></TR>
+<!-- Добавление резины-->
+
+<TR bgcolor="#ebeaef">
+<TD>Изготовление клише из резины</TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Изготовление клише из резины" onClick="price1 = 250; count_price();" id="srok11"> <label for="srok7">250</label></P></TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Изготовление клише из резины" onClick="price1 = 450; count_price();" id="srok12"> <label for="srok8">450</label></P></TD></TR>
+
+
+<!-- конец блока --> 
+<TR bgcolor="#ebeaef">
+<TD>Печать, отрисованная в Corel Draw заказчиком или дубликат до 20 кв. см</TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Печать, отрисованная в Corel Draw заказчиком или дубликат за 1 день" onClick="price1 = 350; count_price();" id="srok7"> <label for="srok7">350</label></P></TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Печать, отрисованная в Corel Draw заказчиком или дубликат срочно" onClick="price1 = 500; count_price();" id="srok8"> <label for="srok8">500</label></P></TD></TR>
+<TR bgcolor="#ebeaef">
+<TD>Маленькие штампы 1-2 слова типа "копия верна", "оплачено"</TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Маленькие штампы 1-2 слова типа 'копия верна', 'оплачено' за 1 день" onClick="price1 = 200; count_price();" id="srok9"> <label for="srok9">200</label></P></TD>
+<TD>
+<P align=center><input type="Radio" name="srok" value="Маленькие штампы 1-2 слова типа 'копия верна', 'оплачено' срочно" onClick="price1 = 400; count_price();" id="srok10"> <label for="srok10">400</label></P></TD></TR>
+<tr>
+		<td colspan="3">
+<a name="uslugi"></a><p align="center"><strong>Дополнительные услуги</strong></p>
+		</td>
+	</tr>
+	<tr bgcolor="#ebeaef" align="center">
+		<td align="left">Отрисовка логотипа, графического изображения в Corel Draw</td>
+		<td>+ 350</td>
+		<td>+ 700</td>
+	</tr>
+</TABLE></P>
