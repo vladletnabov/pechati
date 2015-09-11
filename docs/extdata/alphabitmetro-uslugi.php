ba1e768ba4d63ad84a9f843394ec00bd4590db71
vladletnabov <v.chigikov@yandex.ru>
2015-09-11 10:54:15 +0300

    Обновление списков метро для Марьино

--- docs/extdata/alphabitmetro-uslugi.php
+++ docs/extdata/alphabitmetro-uslugi.php
@@ -35,7 +35,7 @@
 		<td width="33%">		 
 			<p><img src="/pics/line7.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/13.html" title="Изготовление гербовых печатей на Таганке" >м. Таганская</a></p>
 			<!--<p><img src="/pics/line7.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/42.html" title="Срочное Изготовление печатей на Щукинской" >м. Щукинская</a></p>-->
-			<p><img src="/pics/line10.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/6.html" title="" >м. Братиславская</a></p>
+			<p><img src="/pics/line10.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/6.html" title="Печати на заказ в Марьино" >м. Марьино</a></p>
 			<!--<p><img src="/pics/line10.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/46.html" title="" >м. Марьина роща</a></p>-->
 			<p><img src="/pics/line2.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/2.html" title="Печати на заказ м. Речной вокзал">м. Водный стадион</a></p>
 			<p><img src="/pics/line2.gif" width="19" height="9" alt="" align="absmiddle"> <a href="/contacts/41.html" title="Изготовление печатей по оттиску на Динамо"  >м. Динамо</a></p>	    
