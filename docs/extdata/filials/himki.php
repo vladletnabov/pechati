efbf025190f31771f70164fbce783fb97f4f162a
vladletnabov <v.chigikov@yandex.ru>
2016-02-02 13:25:23 +0300

    карта для химок

--- docs/extdata/filials/himki.php
+++ docs/extdata/filials/himki.php
@@ -5,11 +5,15 @@
 	<p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle"> г. Химки<br>ул.Панфилова д.1 (вход со стороны Юбилейного пр-та) Фотосалон<br>Тел.: <strong style="font-size: 8pt;">(495)</strong> 664-37-18</p>
 	<p>Почта: <a href="mailto:zel@pechati.ru">zel@pechati.ru</a></p>
 	<p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle"> г. Химки<br>Станция Химки, ул. Железнодорожная, Платформа №1 (Фотосалон)<br>Тел.: <strong style="font-size: 8pt;">(495)</strong> 664-37-18</p>
+	<p>Почта: <a href="mailto:zel@pechati.ru">zel@pechati.ru</a></p>
     <p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle"> г. Химки<br>Проспект Мельникова д.10 (фотосалон)   <br>Тел.: <strong style="font-size: 8pt;">(495)</strong> 664-37-18</p>
+	<p>Почта: <a href="mailto:zel@pechati.ru">zel@pechati.ru</a></p>
 	<p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle"> г. Химки<br>ул. Московская д.11 (Фотосалон) - (ориентир магазин ЧАСЫ)   <br>Тел.: <strong style="font-size: 8pt;">(495)</strong> 664-37-18</p>
+	<p>Почта: <a href="mailto:zel@pechati.ru">zel@pechati.ru</a></p>
 	
 	<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
     <script src="/extdata/js/yamap-himki.js" type="text/javascript"></script>
-
+	<div style="margin-left:-14px;z-index:999;"><div id="map"></div></div>
+<style>.my-hint{width:246px;height:220px;margin:0;padding:1px;background-color:#fff;border-radius:0; z-index:99999}</style>
 
 </div>
--- docs/extdata/js/yamap-himki.js
+++ docs/extdata/js/yamap-himki.js
@@ -2,16 +2,15 @@ ymaps.ready(init);
 
 function init () {
     var myMap = new ymaps.Map("map", {
-            center: [55.84411074437796, 37.37076478833003],
+            center: [55.9, 37.45],
             zoom: 13
-        }, {
-            searchControlProvider: 'yandex#search'
         }),
     // Создание макета содержимого хинта.
     // Макет создается через фабрику макетов с помощью текстового шаблона.
         HintLayout = ymaps.templateLayoutFactory.createClass( "<div class='my-hint'>" +
             "<b>{{ properties.object }}</b><br />" +
-            "{{ properties.address }}" +
+            "{{ properties.address }}" + 
+			"<img src={{ properties.img }} width='244px'/>" + 
             "</div>", {
                 // Определяем метод getShape, который
                 // будет возвращать размеры макета хинта.
@@ -38,29 +37,40 @@ function init () {
 	
     var myPlacemark = new ymaps.Placemark([55.886731, 37.426959], {
         address: "Химки, ул. Панфилова, д.1 ",
-        object: "Филиал №1 в Химках"
+        object: "Филиал №1 в Химках",
+		img:"/pics/contacts/himki/himki1.png"
     }, {
         hintLayout: HintLayout
     });
     var myPlacemark2 = new ymaps.Placemark([55.894998, 37.449819], {
         address: "Химки, ул. Железнодорожная, Платформа №1 ",
-        object: "Филиал №2 в Химках "
+        object: "Филиал №2 в Химках ",
+		img:"/pics/contacts/himki/himki2.png"
     }, {
         hintLayout: HintLayout
     });
     var myPlacemark3 = new ymaps.Placemark([55.902712, 37.404591], {
         address: "Химки, проспект Мельникова, д.10 (фотосалон) ",
-        object: "Филиал №3 в Химках"
+        object: "Филиал №3 в Химках",
+		img:"/pics/contacts/himki/himki3.png"
     }, {
         hintLayout: HintLayout
     });
     var myPlacemark4 = new ymaps.Placemark([55.890765, 37.44647], {
         address: "Химки, ул. Московская, д.11 (фотосалон) ",
-        object: "Филиал №4 в Химках"
+        object: "Филиал №4 в Химках",
+		img:"/pics/contacts/himki/himki4.png"
     }, {
         hintLayout: HintLayout
     });
 
+	myMap.controls.remove('trafficControl');
+	myMap.controls.remove('typeSelector');
+	myMap.controls.remove('routeEditor');
+	myMap.controls.remove('zoomControl');
+	myMap.controls.remove('smallZoomControl');
+	myMap.controls.remove('scaleLine');
+	myMap.controls.remove('miniMap');
     myMap.geoObjects.add(myPlacemark);
     myMap.geoObjects.add(myPlacemark2);
     myMap.geoObjects.add(myPlacemark3);
