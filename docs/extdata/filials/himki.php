9b89d542a65be0b98a2638618236385e56ed1de5
vladletnabov <v.chigikov@yandex.ru>
2016-02-02 11:32:48 +0300

    Добавление точки в Химках и замена почты для Зеленограда и Химок

--- docs/extdata/filials/himki.php
+++ docs/extdata/filials/himki.php
@@ -2,9 +2,14 @@
 	<!--<div style="float: right;"><a href="javascript: window.print();">Распечатать</a></div>
 	<p>Бесплатная доставка печатей и штампов в г. Химки и Новые Химки</p>
 	<p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle">-->
-	<p>	г. Химки<br>ул.Панфилова д.1 (вход со стороны Юбилейного пр-та) Фотосалон<br>Тел.: <strong style="font-size: 8pt;">(495)</strong> 664-37-18</p>
-	<p>Почта: <a href="mailto:6643718@mail.ru">6643718@mail.ru</a></p>
+	<p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle"> г. Химки<br>ул.Панфилова д.1 (вход со стороны Юбилейного пр-та) Фотосалон<br>Тел.: <strong style="font-size: 8pt;">(495)</strong> 664-37-18</p>
+	<p>Почта: <a href="mailto:zel@pechati.ru">zel@pechati.ru</a></p>
 	<p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle"> г. Химки<br>Станция Химки, ул. Железнодорожная, Платформа №1 (Фотосалон)<br>Тел.: <strong style="font-size: 8pt;">(495)</strong> 664-37-18</p>
     <p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle"> г. Химки<br>Проспект Мельникова д.10 (фотосалон)   <br>Тел.: <strong style="font-size: 8pt;">(495)</strong> 664-37-18</p>
+	<p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle"> г. Химки<br>ул. Московская д.11 (Фотосалон) - (ориентир магазин ЧАСЫ)   <br>Тел.: <strong style="font-size: 8pt;">(495)</strong> 664-37-18</p>
+	
+	<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+    <script src="/extdata/js/yamap-himki.js" type="text/javascript"></script>
+
 
 </div>
---
+++ docs/extdata/js/yamap-himki.js
@@ -0,0 +1,68 @@
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map("map", {
+            center: [55.84411074437796, 37.37076478833003],
+            zoom: 13
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+    // Создание макета содержимого хинта.
+    // Макет создается через фабрику макетов с помощью текстового шаблона.
+        HintLayout = ymaps.templateLayoutFactory.createClass( "<div class='my-hint'>" +
+            "<b>{{ properties.object }}</b><br />" +
+            "{{ properties.address }}" +
+            "</div>", {
+                // Определяем метод getShape, который
+                // будет возвращать размеры макета хинта.
+                // Это необходимо для того, чтобы хинт автоматически
+                // сдвигал позицию при выходе за пределы карты.
+                getShape: function () {
+                    var el = this.getElement(),
+                        result = null;
+                    if (el) {
+                        var firstChild = el.firstChild;
+                        result = new ymaps.shape.Rectangle(
+                            new ymaps.geometry.pixel.Rectangle([
+                                [0, 0],
+                                [firstChild.offsetWidth, firstChild.offsetHeight]
+                            ])
+                        );
+                    }
+                    return result;
+                }
+            }
+        );
+	
+    
+	
+    var myPlacemark = new ymaps.Placemark([55.886731, 37.426959], {
+        address: "Химки, ул. Панфилова, д.1 ",
+        object: "Филиал №1 в Химках"
+    }, {
+        hintLayout: HintLayout
+    });
+    var myPlacemark2 = new ymaps.Placemark([55.894998, 37.449819], {
+        address: "Химки, ул. Железнодорожная, Платформа №1 ",
+        object: "Филиал №2 в Химках "
+    }, {
+        hintLayout: HintLayout
+    });
+    var myPlacemark3 = new ymaps.Placemark([55.902712, 37.404591], {
+        address: "Химки, проспект Мельникова, д.10 (фотосалон) ",
+        object: "Филиал №3 в Химках"
+    }, {
+        hintLayout: HintLayout
+    });
+    var myPlacemark4 = new ymaps.Placemark([55.890765, 37.44647], {
+        address: "Химки, ул. Московская, д.11 (фотосалон) ",
+        object: "Филиал №4 в Химках"
+    }, {
+        hintLayout: HintLayout
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+    myMap.geoObjects.add(myPlacemark2);
+    myMap.geoObjects.add(myPlacemark3);
+    myMap.geoObjects.add(myPlacemark4);
+}
