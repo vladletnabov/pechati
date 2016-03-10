3d148a170b94c79d1db41e8ef88aec3da8502acd
vladletnabov <v.chigikov@yandex.ru>
2016-03-10 11:21:02 +0300

    Убрать адрес и фото

--- docs/extdata/filials/novokosino.php
+++ docs/extdata/filials/novokosino.php
@@ -1,7 +1,7 @@
 <div id="contacts1">
 	<!--<div style="float: right;"><a href="javascript: window.print();">Распечатать</a></div>-->
 	<p><img src="../i/0.gif" class="poi2" width="13" height="13" alt="" align="absmiddle">Печати и штампы м. Новокосино<br>
-	ул.Новокосинская 17<!--, к7, вход в "Аптеку"--><br>
+	<!--ул.Новокосинская 17--><!--, к7, вход в "Аптеку"--><br>
 	<a href="mailto:perovo@pechati.ru">perovo@pechati.ru</a><br>
 	Тел.: <strong>(926)</strong>  845-61-71<br />
 	<strong>(916)</strong> 232-80-30<br />
@@ -10,9 +10,9 @@
 	<b>Режим работы:</b> Пн-Вс с 10:00 - 20:00 без выходных.</p>
 	
 	<!--<img alt="печати и штампы у м. Новокосино" src="/pics/contacts/novokosino/0.jpg">-->
-	<div align="center" id="pics">
-	<!--<img alt="печати и штампы у м. Новокосино" src="/pics/contacts/novokosino/1.jpg"><br />-->
+	<!--<div align="center" id="pics">-->
+	<!--<img alt="печати и штампы у м. Новокосино" src="/pics/contacts/novokosino/1.jpg"><br />
 	<img alt="печати и штампы у м. Новокосино" src="/pics/contacts/novokosino/2.jpg"><br />
 	<img alt="печати и штампы у м. Новокосино" src="/pics/contacts/novokosino/3.jpg">
-	</div>
+	</div>-->
 </div>
