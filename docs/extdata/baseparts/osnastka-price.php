6c3eea723e7da9323240ebea4fecc3732fa3095e
vladletnabov <v.chigikov@yandex.ru>
2016-03-28 10:37:44 +0300

    Список филиалов в три колонки, дистанция между оснастками

--- docs/extdata/baseparts/osnastka-price.php
+++ docs/extdata/baseparts/osnastka-price.php
@@ -1,67 +1,3 @@
-<!--<script>
-var price2 = 450;
-</script>-->
-<!--<P><img src="../pics/str.gif" width="5" height="9" alt=""> <B>Наиболее популярные модели оснастки</B><BR>-->
-
-<!--<p><a style="font-size: 20px; color: navy;">Шаг 2: Выберите оснастку</a><br>(Нажмите на изображение для просмотра расширенного ассортимента)</P>-->
-<!--<P align="center"><img src="../pics/str.gif" width="5" height="9" alt=""> <B>Наиболее популярные модели оснастки</B><BR>(Нажмите на изображение для просмотра расширенного ассортимента)</P>-->
-<!--<table cellpadding="10" cellspacing="0" border="0" align="center">
-	<tr valign="top" align="center">
-		<td width="25%">
-<A href="javascript:void(window.open('http://www.pechati.ru/uslugi/osn_automat.html','','resizable=no,location=no,menubar=no,scrollbars=yes,status=no,toolbar=no,fullscreen=no,dependent=no,width=690,height=750,left=1,top=1'))"><IMG border=0 src="../pics/uslugi/colop_printer_r40_100x133.jpg" width=100 height=133></A>
-<p>Автоматическая оснастка<BR>Диам. 40 мм.<BR><input type="Radio" name="osnast" checked value="Автоматическая оснастка Диам. 40 мм." onClick="price2 = 450; count_price();" id="osnast1"> <label for="osnast1">450 руб.</label></p>
-		</td>
-		<td width="25%">
-<A href="javascript:void(window.open('http://www.pechati.ru/uslugi/osn_automat_mal.html','','resizable=no,location=no,menubar=no,scrollbars=yes,status=no,toolbar=no,fullscreen=no,dependent=no,width=690,height=800,left=1,top=1'))"><IMG border=0 src="../pics/uslugi/shiny-s-843.jpg" width=100 height=133></A>
-<p>Автоматическая оснастка<BR>для мал. штампов<BR><input type="Radio" name="osnast" value="Автоматическая оснастка для мал. штампов" onClick="price2 = 300; count_price();" id="osnast2"> <label for="osnast2">300 руб.</label></p>
-		</td>
-		<td width="25%">
-<A href="javascript:void(window.open('http://www.pechati.ru/uslugi/osn_automat_bolsh.html','','resizable=no,location=no,menubar=no,scrollbars=yes,status=no,toolbar=no,fullscreen=no,dependent=no,width=690,height=750,left=1,top=1'))"><IMG border=0 src="../pics/uslugi/grm-4927.jpg" width=100 height=133></A>
-<p>Автоматическая оснастка<BR>для больших штампов<BR><input type="Radio" name="osnast" value="Автоматическая оснастка для больших штампов" onClick="price2 = 500; count_price();" id="osnast3"> <label for="osnast3">500 руб.</label></p>
-		</td>
-		<td width="25%">
-<A href="javascript:void(window.open('http://www.pechati.ru/uslugi/osn_profline.html','','resizable=no,location=no,menubar=no,scrollbars=yes,status=no,toolbar=no,fullscreen=no,dependent=no,width=690,height=750,left=1,top=1'))"><IMG border=0 src="../pics/uslugi/colop-r3040.jpg" width=100 height=133></A>
-<p>Colop R 3040<BR>Диам. 45 мм.<BR><input type="Radio" name="osnast" value="Professional Line 5215 Диам. 45 мм." onClick="price2 = 1000; count_price();" id="osnast4"> <label for="osnast4">от 1500 руб.</label></p>
-		</td>
-	</tr>
-	<tr valign="top" align="center">
-		<td>
-<A href="javascript:void(window.open('http://www.pechati.ru/uslugi/metal_osn_pech_stamp.html','','resizable=no,location=no,menubar=no,scrollbars=yes,status=no,toolbar=no,fullscreen=no,dependent=no,width=690,height=800,left=1,top=1'))"><IMG border=0 src="../pics/uslugi/met_classic_100x133.jpg" width=100 height=133></A>
-<p>Металлические оснастки<BR>для печатей и штампов<BR><input type="Radio" name="osnast" value="Металлические оснастки для печатей и штампов" onClick="price2 = 800; count_price();" id="osnast5"> <label for="osnast5">от 800 р.</label></p>
-		</td>
-		<td>
-<A href="javascript:void(window.open('http://www.pechati.ru/uslugi/metal_osn_original.html','','resizable=no,location=no,menubar=no,scrollbars=yes,status=no,toolbar=no,fullscreen=no,dependent=no,width=690,height=1000,left=1,top=1'))"><IMG border=0 src="../pics/uslugi/met_techno_100x133.jpg" width=100 height=133></A>
-<p>Эксклюзивные оснастки<BR><input type="Radio" name="osnast" value="Оригинальные оснастки" onClick="price2 = 1500; count_price();" id="osnast6"> <label for="osnast6">1500 р.</label></p>
-		</td><td>
-<A href="javascript:void(window.open('http://www.pechati.ru/uslugi/plastm_osn_gerb.html','','resizable=no,location=no,menubar=no,scrollbars=yes,status=no,toolbar=no,fullscreen=no,dependent=no,width=690,height=1000,left=1,top=1'))"><IMG border=0 src="../pics/uslugi/plasm_gerb40_100x133.jpg" width=100 height=133></A>
-<p>Оснастка с гербом<BR>Диам. 40 мм.<BR><input type="Radio" name="osnast" value="Оснастка с гербом Диам. 40 мм." onClick="price2 = 0; count_price();" id="osnast7"> <label for="osnast7">Бесплатно.</label></p>
-		</td><td>
-<A href="javascript:void(window.open('http://www.pechati.ru/uslugi/elit_osn.html','','resizable=no,location=no,menubar=no,scrollbars=yes,status=no,toolbar=no,fullscreen=no,dependent=no,width=690,height=1000,left=1,top=1'))"><IMG border=0 src="../pics/uslugi/elit_osn.jpg" width=100 height=133></A>
-<p>Элитные оснастки<BR>от 5500 р.</p></td>
-	</tr>
-</table>-->
-
-
-    <!--<script src="/extdata/osnastka-slider/js/jquery.js"></script>
-    <script src="/extdata/osnastka-slider/js/amazingslider.js"></script>
-    <link rel="stylesheet" type="text/css" href="/extdata/osnastka-slider/js/amazingslider-1.css">
-    <script src="/extdata/osnastka-slider/js/initslider-1.js"></script>
-	<link rel="stylesheet" type="text/css" href="/extdata/osnastka-slider/js/amazingslider-2.css">
-    <script src="/extdata/osnastka-slider/js/initslider-2.js"></script>
-	<link rel="stylesheet" type="text/css" href="/extdata/osnastka-slider/js/amazingslider-3.css">
-    <script src="/extdata/osnastka-slider/js/initslider-3.js"></script>
-	<link rel="stylesheet" type="text/css" href="/extdata/osnastka-slider/js/amazingslider-4.css">
-    <script src="/extdata/osnastka-slider/js/initslider-4.js"></script>
-	<link rel="stylesheet" type="text/css" href="/extdata/osnastka-slider/js/amazingslider-5.css">
-    <script src="/extdata/osnastka-slider/js/initslider-5.js"></script>
-	<link rel="stylesheet" type="text/css" href="/extdata/osnastka-slider/js/amazingslider-6.css">
-    <script src="/extdata/osnastka-slider/js/initslider-6.js"></script>
-	<link rel="stylesheet" type="text/css" href="/extdata/osnastka-slider/js/amazingslider-7.css">
-    <script src="/extdata/osnastka-slider/js/initslider-7.js"></script>
-	<link rel="stylesheet" type="text/css" href="/extdata/osnastka-slider/js/amazingslider-8.css">
-    <script src="/extdata/osnastka-slider/js/initslider-8.js"></script>-->
-
-
 
 <script>
 var price2 = 450;
@@ -153,7 +89,7 @@ var price2 = 450;
 }
 </style>
 
-<table cellpadding="10" cellspacing="0" border="0" align="center">
+<table cellpadding="10" cellspacing="0" border="0" align="center" width="800">
 	<tr valign="top" align="center">
 		<td width="100%">
 			<div style="clear:both; width:100%; margin:0;padding:0">
