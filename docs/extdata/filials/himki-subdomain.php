553483de465c2c66c248dad4c4d737dd9baa8b1d
vladletnabov <v.chigikov@yandex.ru>
2016-08-08 22:17:02 +0300

    Добавлена информация по Химкам

---
+++ docs/extdata/filials/himki-subdomain.php
@@ -0,0 +1,46 @@
+<h3>График работы</h3>
+
+<p>Понедельник	09:00 - 18:00</p>
+<p>Вторник	09:00 - 20:00</p>
+<p>Среда	09:00 - 18:00</p>
+<p>Четверг	09:00 - 20:00</p>
+<p>Пятница	09:00 - 18:00</p>
+<p>Суббота	10:00 - 15:00 (первая, третья)</p>
+<p>Воскресенье	выходной</p>
+
+<p>Адрес: г. Химки, Нагорное шоссе, дом 2, офис 18А, второй этаж (вход через проходную в доме №2)</p>
+
+<script src="/extdata/slider/js/jquery.js"></script>
+    <script src="/extdata/slider/js/amazingslider.js"></script>
+    <link rel="stylesheet" type="text/css" href="/extdata/slider/js/amazingslider-63.css">
+    <script src="/extdata/slider/js/initslider-63.js"></script>
+	
+	
+    
+	<div id="amazingslider-wrapper-1" style="display:block;position:relative;max-width:600px;padding-left:0px; padding-right:244px;margin:0px auto 0px;">
+        <div id="amazingslider-1" style="display:block;position:relative;margin:0 auto;">
+            <ul class="amazingslider-slides" style="display:none;">			
+                <li><img src="http://pechati.ru/pics/contacts/himki2/1.jpeg" alt="1. Печати.РУ в Химках"  title="1. Печати.РУ в Химках" data-description="Печати.РУ в Химках" /></li>
+				<li><img src="http://pechati.ru/pics/contacts/himki2/2.jpeg" alt="2. Печати.РУ в Химках"  title="2. Печати.РУ в Химках" data-description="Печати.РУ в Химках" /></li>
+				<li><img src="http://pechati.ru/pics/contacts/himki2/3.jpeg" alt="3. Печати.РУ в Химках"  title="3. Печати.РУ в Химках" data-description="Печати.РУ в Химках" /></li>
+				<li><img src="http://pechati.ru/pics/contacts/himki2/4.jpeg" alt="4. Печати.РУ в Химках"  title="4. Печати.РУ в Химках" data-description="Печати.РУ в Химках" /></li>
+				<li><img src="http://pechati.ru/pics/contacts/himki2/5.jpeg" alt="5. Печати.РУ в Химках"  title="5. Печати.РУ в Химках" data-description="Печати.РУ в Химках" /></li>
+				<li><img src="http://pechati.ru/pics/contacts/himki2/6.jpeg" alt="6. Печати.РУ в Химках"  title="6. Печати.РУ в Химках" data-description="Печати.РУ в Химках" /></li>
+				<li><img src="http://pechati.ru/pics/contacts/himki2/7.jpeg" alt="7. Печати.РУ в Химках"  title="7. Печати.РУ в Химках" data-description="Печати.РУ в Химках" /></li>
+				<li><img src="http://pechati.ru/pics/contacts/himki2/8.jpeg" alt="8. Печати.РУ в Химках"  title="8. Печати.РУ в Химках" data-description="Печати.РУ в Химках" /></li>
+				
+            </ul>
+            <ul class="amazingslider-thumbnails" style="display:none;">
+                <li><img src="http://pechati.ru/pics/contacts/himki2/1-tn.jpeg" alt="1. Печати.РУ в Химках"  title="1. Печати.РУ в Химках"  /></li>
+                <li><img src="http://pechati.ru/pics/contacts/himki2/2-tn.jpeg" alt="2. Печати.РУ в Химках"  title="2. Печати.РУ в Химках"  /></li>
+                <li><img src="http://pechati.ru/pics/contacts/himki2/3-tn.jpeg" alt="3. Печати.РУ в Химках"  title="3. Печати.РУ в Химках"  /></li>
+                <li><img src="http://pechati.ru/pics/contacts/himki2/4-tn.jpeg" alt="4. Печати.РУ в Химках"  title="4. Печати.РУ в Химках"  /></li>
+                <li><img src="http://pechati.ru/pics/contacts/himki2/5-tn.jpeg" alt="5. Печати.РУ в Химках"  title="5. Печати.РУ в Химках"  /></li>
+                <li><img src="http://pechati.ru/pics/contacts/himki2/6-tn.jpeg" alt="6. Печати.РУ в Химках"  title="6. Печати.РУ в Химках"  /></li>
+                <li><img src="http://pechati.ru/pics/contacts/himki2/7-tn.jpeg" alt="7. Печати.РУ в Химках"  title="7. Печати.РУ в Химках"  /></li>
+                <li><img src="http://pechati.ru/pics/contacts/himki2/8-tn.jpeg" alt="8. Печати.РУ в Химках"  title="8. Печати.РУ в Химках"  /></li>
+            </ul>
+        <div class="amazingslider-engine"><a href="http://amazingslider.com" title="Responsive jQuery Image Slideshow">Responsive jQuery Image Slideshow</a></div>
+        </div>
+    </div>
+<br><br>
