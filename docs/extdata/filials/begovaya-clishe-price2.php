9eb57ce4db84ab3f57b448d32eb771d8905df22a
egorbunov96 <egorbunov96@narod.ru>
2016-10-31 23:56:20 +0300

    Изменил цены на Беговой
    поменял местами (теперь от меньшего к большему) и изменил цены

--- docs/extdata/filials/begovaya-clishe-price2.php
+++ docs/extdata/filials/begovaya-clishe-price2.php
@@ -20,13 +20,13 @@ var price1 = 1050;
 		<TD>
 			<a name="klishe"></a><!--<P align=center><B>Клише из резины. </B></P>--></TD>
 		<TD>
-			<P align=center><B>Срочно<br />(до 2 часов)</B></P>
+		<P align=center><B>На<br />следующий<br />день </B></P>	
 		</TD>
 		<TD>
 			<P align=center><B>Сегодня<br />(от 4 часов)</B></P>
 		</TD>
 		<TD>
-			<P align=center><B>На<br />следующий<br />день </B></P>
+		<P align=center><B>Срочно<br />(до 2 часов)</B></P>
 		</TD>
 	</TR>
 	<TR bgcolor="#ebeaef">
@@ -42,167 +42,167 @@ var price1 = 1050;
 		</TD>
 	</TR>
 	<TR bgcolor="#ebeaef">
-		<TD><strong>Новая печать </strong> (резина)</TD>
-		<TD>
-			<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону срочно за час" onClick="price1 = 1050; count_price();" id="clishe2a"> <label for="clishe2a">1 050 р</label></P>
-		</TD>
-		<TD>
-			<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону срочно в течении дня" onClick="price1 = 900; count_price();" id="clishe2b"> <label for="clishe2b">900 р</label></P>
-		</TD>
-		<TD>
-			<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону за день" onClick="price1 = 750; count_price();" id="clishe2c"> <label for="clishe2c">750 р</label></P>
-		</TD>
-	</TR>
-	<TR bgcolor="#ebeaef">
 		<TD><strong>Новая печать </strong> (фотополимер)</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону срочно за час" onClick="price1 = 850; count_price();" id="clishe3a"> <label for="clishe3a">850 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону за день" onClick="price1 = 400; count_price();" id="clishe3a"> <label for="clishe3a">400 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону срочно в течении дня" onClick="price1 = 700; count_price();" id="clishe3b"> <label for="clishe3b">700 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону срочно в течении дня" onClick="price1 = 550; count_price();" id="clishe3b"> <label for="clishe3b">550 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону за день" onClick="price1 = 550; count_price();" id="clishe3c"> <label for="clishe3c">550 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону срочно за час" onClick="price1 = 750; count_price();" id="clishe3c"> <label for="clishe3c">750 р</label></P>
 		</TD>
 	</TR>
-	<TR bgcolor="#ebeaef">
-		<TD><strong>Печать по оттиску</strong> (резина)</TD>
+		<TR bgcolor="#ebeaef">
+		<TD><strong>Новая печать </strong> (резина)</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Печать по оттиску (резина) срочно за час" onClick="price1 = 1600; count_price();" id="clishe4a"> <label for="clishe4a">1 600 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону за день" onClick="price1 = 600; count_price();" id="clishe2a"> <label for="clishe2a">600 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Печать по оттиску (резина) срочно в течении дня" onClick="price1 = 1300; count_price();" id="clishe4b"> <label for="clishe4b">1 300 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону срочно в течении дня" onClick="price1 = 750; count_price();" id="clishe2b"> <label for="clishe2b">750 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Печать по оттиску (резина) за день" onClick="price1 = 1050; count_price();" id="clishe4c"> <label for="clishe4c">1 050 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Новые печати, штампы по шаблону срочно за час" onClick="price1 = 950; count_price();" id="clishe2c"> <label for="clishe2c">950 р</label></P>
 		</TD>
 	</TR>
 	<TR bgcolor="#ebeaef">
 		<TD><strong>Печать по оттиску</strong> (фотополимер)</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Печать по оттиску (фотополимер) срочно за час" onClick="price1 = 1400; count_price();" id="clishe5a"> <label for="clishe5a">1 400 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Печать по оттиску (фотополимер) за день" onClick="price1 = 700; count_price();" id="clishe5a"> <label for="clishe5a">700 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Печать по оттиску (фотополимер) срочно в течении дня" onClick="price1 = 1100; count_price();" id="clishe5b"> <label for="clishe5b">1 100 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Печать по оттиску (фотополимер) срочно в течении дня" onClick="price1 = 900; count_price();" id="clishe5b"> <label for="clishe5b">900 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Печать по оттиску (фотополимер) за день" onClick="price1 = 850; count_price();" id="clishe5c"> <label for="clishe5c">850 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Печать по оттиску (фотополимер) срочно за час" onClick="price1 = 1150; count_price();" id="clishe5c"> <label for="clishe5c">1150 р</label></P>
 		</TD>
 	</TR>
 	<TR bgcolor="#ebeaef">
-		<TD><strong>Факсимиле</strong> (резина)</TD>
+		<TD><strong>Печать по оттиску</strong> (резина)</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Факсимиле срочно за час" onClick="price1 = 1150; count_price();" id="clishe6a"> 
-			<label for="clishe6a">1 150 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Печать по оттиску (резина) за день" onClick="price1 = 900; count_price();" id="clishe4a"> <label for="clishe4a">900 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Факсимиле срочно в течении дня" onClick="price1 = 1000; count_price();" id="clishe6b"> 
-			<label for="clishe6b">1 000 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Печать по оттиску (резина) срочно в течении дня" onClick="price1 = 1100; count_price();" id="clishe4b"> <label for="clishe4b">1100 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Факсимилеза день" onClick="price1 = 850; count_price();" id="clishe6c"> 
-			<label for="clishe6c">850 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Печать по оттиску (резина) срочно за час" onClick="price1 = 1350; count_price();" id="clishe4c"> <label for="clishe4c">1350 р</label></P>
 		</TD>
 	</TR>
 	<TR bgcolor="#ebeaef">
 		<TD><strong>Факсимиле</strong> (фотополимер)</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Факсимиле срочно за час" onClick="price1 = 950; count_price();" id="clishe7a"> 
-			<label for="clishe7a">950 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Факсимиле за день" onClick="price1 = 550; count_price();" id="clishe7a"> 
+			<label for="clishe7a">550 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Факсимиле срочно в течении дня" onClick="price1 = 800; count_price();" id="clishe7b"> 
-			<label for="clishe7b">800 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Факсимиле срочно в течении дня" onClick="price1 = 650; count_price();" id="clishe7b"> 
+			<label for="clishe7b">650 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Факсимиле за день" onClick="price1 = 650; count_price();" id="clishe7c"> 
-			<label for="clishe7c">650 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Факсимиле срочно за час" onClick="price1 = 800; count_price();" id="clishe7c"> 
+			<label for="clishe7c">800 р</label></P>
+		</TD>
+	</TR>
+	<TR bgcolor="#ebeaef">
+		<TD><strong>Факсимиле</strong> (резина)</TD>
+		<TD>
+			<P align=center><input type="Radio" name="srok" value="Факсимиле за день" onClick="price1 = 750; count_price();" id="clishe6a"> 
+			<label for="clishe6a">750 р</label></P>
+		</TD>
+		<TD>
+			<P align=center><input type="Radio" name="srok" value="Факсимиле срочно в течении дня" onClick="price1 = 850; count_price();" id="clishe6b"> 
+			<label for="clishe6b">850 р</label></P>
+		</TD>
+		<TD>
+			<P align=center><input type="Radio" name="srok" value="Факсимиле срочно за час" onClick="price1 = 1000; count_price();" id="clishe6c"> 
+			<label for="clishe6c">1000 р</label></P>
 		</TD>
 	</TR>
 	<TR bgcolor="#ebeaef">
 		<TD><strong>Детская печать</strong> (резина)</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Детская печать (резина) срочно за час" onClick="price1 = 1050; count_price();" id="clishe8a"> 
-			<label for="clishe8a">1 050 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Детская печать (резина) за день" onClick="price1 = 600; count_price();" id="clishe8a"> 
+			<label for="clishe8a">600 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Детская печать (резина) срочно в течении дня" onClick="price1 = 900; count_price();" id="clishe8b"> 
-			<label for="clishe8b">900 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Детская печать (резина) срочно в течении дня" onClick="price1 = 750; count_price();" id="clishe8b"> 
+			<label for="clishe8b">750 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Детская печать (резина) за день" onClick="price1 = 750; count_price();" id="clishe8c"> 
-			<label for="clishe8c">750 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Детская печать (резина) срочно за час" onClick="price1 = 950; count_price();" id="clishe8c"> 
+			<label for="clishe8c">950 р</label></P>
 		</TD>
 	</TR>
 	<TR bgcolor="#ebeaef">
 		<TD><strong>Флеш печать</strong> (новая)</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Флеш печать (новая) срочно за час" onClick="price1 = 1200; count_price();" id="clishe9a"> 
-			<label for="clishe9a">1 200 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Флеш печать (новая) за день" onClick="price1 = 900; count_price();" id="clishe9a"> 
+			<label for="clishe9a">900 р</label></P>
 		</TD>
 		<TD>
 			<P align=center><input type="Radio" name="srok" value="Флеш печать (новая) срочно в течении дня" onClick="price1 = 1050; count_price();" id="clishe9b"> 
 			<label for="clishe9b">1050 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Флеш печать (новая) за день" onClick="price1 = 900; count_price();" id="clishe9c"> 
-			<label for="clishe9c">900 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Флеш печать (новая) срочно за час" onClick="price1 = 1200; count_price();" id="clishe9c"> 
+			<label for="clishe9c">1200 р</label></P>
 		</TD>
 	</TR>
 	<TR bgcolor="#ebeaef">
 		<TD><strong>Флеш печать</strong> (по оттиску)</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Флеш печать (по оттиску) срочно за час" onClick="price1 = 1500; count_price();" id="clishe10a"> 
-			<label for="clishe10a">1 500 р</label></P>
+			<P align=center><input type="Radio" name="srok" value="Флеш печать (по оттиску) за день" onClick="price1 = 1200; count_price();" id="clishe10a"> 
+			<label for="clishe10a">1200 р</label></P>
 		</TD>
 		<TD>
 			<P align=center><input type="Radio" name="srok" value="Флеш печать (по оттиску) срочно в течении дня" onClick="price1 = 1350; count_price();" id="clishe10b"> 
 			<label for="clishe10b">1350 р</label></P>
 		</TD>
 		<TD>
-			<P align=center><input type="Radio" name="srok" value="Флеш печать (по оттиску) за день" onClick="price1 = 1200; count_price();" id="clishe10c"> 
-			<label for="clishe10c">1200 р</label></P>
-		</TD>
-	</TR>
-	<TR bgcolor="#ebeaef">
-		<TD><strong>Печать, отрисованная в кривых</strong> (резина)</TD>
-		<TD>
-			<P align=center>
-				<input type="Radio" name="srok" value="Печать, отрисованная в кривых (резина) срочно за час" onClick="price1 = 550; count_price();" id="clishe11a"> 
-				<label for="clishe11a">550 р</label>
-			</P>
-		</TD>
-		<TD>
-			<P align=center>
-				<input type="Radio" name="srok" value="Печать, отрисованная в кривых (резина) срочно в течении дня" onClick="price1 = 550; count_price();" id="clishe11b"> 
-				<label for="clishe11b">550 р</label>
-			</P>
-		</TD>
-		<TD>
-			<P align=center>
-				<input type="Radio" name="srok" value="Печать, отрисованная в кривых (резина) за день" onClick="price1 = 550; count_price();" id="clishe11c"> 
-				<label for="clishe11c">550 р</label>
-			</P>
+			<P align=center><input type="Radio" name="srok" value="Флеш печать (по оттиску) срочно за час" onClick="price1 = 1500; count_price();" id="clishe10c"> 
+			<label for="clishe10c">1500 р</label></P>
 		</TD>
 	</TR>
 	<TR bgcolor="#ebeaef">
 		<TD><strong>Печать, отрисованная в кривых</strong> (фотополимер)</TD>
 		<TD>
 			<P align=center>
-				<input type="Radio" name="srok" value="Печать, отрисованная в кривых (фотополимер) срочно за час" onClick="price1 = 350; count_price();" id="clishe12a"> 
-				<label for="clishe12a">350 р</label>
+				<input type="Radio" name="srok" value="Печать, отрисованная в кривых (фотополимер) за день" onClick="price1 = 300; count_price();" id="clishe12a"> 
+				<label for="clishe12a">300 р</label>
 			</P>
 		</TD>
 		<TD>
 			<P align=center>
-				<input type="Radio" name="srok" value="Печать по ГОСТ срочно в течении дня" onClick="price1 = 350; count_price();" id="clishe12b"> 
-				<label for="clishe12b">350 р</label>
+				<input type="Radio" name="srok" value="Печать, отрисованная в кривых (фотополимер) в течении дня" onClick="price1 = 300; count_price();" id="clishe12b"> 
+				<label for="clishe12b">300 р</label>
 			</P>
 		</TD>
 		<TD>
 			<P align=center>
-				<input type="Radio" name="srok" value="Печать по ГОСТ за день" onClick="price1 = 350; count_price();" id="clishe12c"> 
-				<label for="clishe12c">350 р</label>
+				<input type="Radio" name="srok" value="Печать, отрисованная в кривых (фотополимер) срочно за час" onClick="price1 = 300; count_price();" id="clishe12c"> 
+				<label for="clishe12c">300 р</label>
+			</P>
+		</TD>
+	</TR>
+	<TR bgcolor="#ebeaef">
+		<TD><strong>Печать, отрисованная в кривых</strong> (резина)</TD>
+		<TD>
+			<P align=center>
+				<input type="Radio" name="srok" value="Печать, отрисованная в кривых (резина) за день" onClick="price1 = 500; count_price();" id="clishe11a"> 
+				<label for="clishe11a">500 р</label>
+			</P>
+		</TD>
+		<TD>
+			<P align=center>
+				<input type="Radio" name="srok" value="Печать, отрисованная в кривых (резина) срочно в течении дня" onClick="price1 = 500; count_price();" id="clishe11b"> 
+				<label for="clishe11b">500 р</label>
+			</P>
+		</TD>
+		<TD>
+			<P align=center>
+				<input type="Radio" name="srok" value="Печать, отрисованная в кривых (резина) срочно за час" onClick="price1 = 500; count_price();" id="clishe11c"> 
+				<label for="clishe11c">500 р</label>
 			</P>
 		</TD>
 	</TR>
@@ -225,29 +225,29 @@ var price1 = 1050;
 		</TD>
 	</TR>
 	<TR bgcolor="#ebeaef">
-		<TD><strong>Штампы малого размера</strong> *</TD>
+		<TD><strong>Штампы новые малого размера</strong> *</TD>
 		<TD colspan="3">
 			<P align=center>
-				<input type="Radio" name="srok" value="Штампы малого размера срочно" onClick="price1 = 300; count_price();" id="clishe15a"> 
-				<label for="clishe15a">300 р</label>
+				<input type="Radio" name="srok" value="Штампы малого размера срочно" onClick="price1 = 200; count_price();" id="clishe15a"> 
+				<label for="clishe15a">от 200 р</label>
 			</P>
 		</TD>
 	</TR>
 	<TR bgcolor="#ebeaef">
-		<TD><strong>Штампы среднего размера</strong> *</TD>
+		<TD><strong>Штампы новые среднего размера</strong> *</TD>
 		<TD colspan="3">
 			<P align=center>
-				<input type="Radio" name="srok" value="Штампы среднего размера срочно" onClick="price1 = 550; count_price();" id="clishe16a"> 
-				<label for="clishe16a">550 р</label>
+				<input type="Radio" name="srok" value="Штампы среднего размера срочно" onClick="price1 = 400; count_price();" id="clishe16a"> 
+				<label for="clishe16a">от 400 р</label>
 			</P>
 		</TD>
 	</TR>
 	<TR bgcolor="#ebeaef">
-		<TD><strong>Штампы большого размера</strong> *</TD>
+		<TD><strong>Штампы новые большого размера</strong> *</TD>
 		<TD colspan="3">
 			<P align=center>
-				<input type="Radio" name="srok" value="Штампы большого размера срочно" onClick="price1 = 850; count_price();" id="clishe17a"> 
-				<label for="clishe17a">850 р</label>
+				<input type="Radio" name="srok" value="Штампы большого размера срочно" onClick="price1 = 650; count_price();" id="clishe17a"> 
+				<label for="clishe17a">от 650 р</label>
 			</P>
 		</TD>
 	</TR>
@@ -266,31 +266,42 @@ var price1 = 1050;
 		<td align="left">Отрисовка логотипа, графического изображения в Corel Draw</td>
 		<td colspan="3">
 			<script>
-			var otrisovka=300;
+			var otrisovka=200;
 			var priceotrisovka=0;
 			</script>
-			<P align=center><input type="checkbox" name="otrisov" value="Отрисовка логотипа, графического изображения"  onClick="countotrisovka(); count_price();" id="otrisovka1"> <label for="otrisovka1">300</label></P>
+			<P align=center><input type="checkbox" name="otrisov" value="Отрисовка логотипа, графического изображения"  onClick="countotrisovka(); count_price();" id="otrisovka1"> <label for="otrisovka1">200</label></P>
 		</td>
 	</tr>
 	<tr bgcolor="#ebeaef" align="center">
 		<td align="left">Простые элементы защиты: окантовка, микродефекты</td>
 		<td colspan="3">
 		<script>
-			var zaschita=200;
+			var zaschita=100;
 			var pricezaschit=0;
 			</script>
-			<P align=center><input type="checkbox" name="zaschit" value="Отрисовка логотипа, графического изображения"  onClick="countzaschita()" id="zaschita1"> <label for="zaschit1">200</label></P>
+			<P align=center><input type="checkbox" name="zaschit" value="Отрисовка логотипа, графического изображения"  onClick="countzaschita()" id="zaschita1"> <label for="zaschit1">100</label></P>
 		
 		</td>
 	</tr>
 	<tr bgcolor="#ebeaef" align="center">
-		<td align="left">Доставка</td>
+		<td align="left">Доставка в шаговой доступности от метро</td>
 		<td colspan="3">
 		<script>
-			var dostavka=300;
+			var dostavka=150;
 			var pricedostav=0;
 			</script>
-			<P align=center><input type="checkbox" name="dostav" value="Отрисовка логотипа, графического изображения"  onClick="countdostavka(); count_price();" id="dostavka1"> <label for="dostav1">300</label></P>
+			<P align=center><input type="checkbox" name="dostav" value="Отрисовка логотипа, графического изображения"  onClick="countdostavka(); count_price();" id="dostavka1"> <label for="dostav1">150</label></P>
+		
+		</td>
+	</tr>
+	<tr bgcolor="#ebeaef" align="center">
+		<td align="left">Доставка до метро</td>
+		<td colspan="3">
+		<script>
+			var dostavka=бесплатно;
+			var pricedostav=0;
+			</script>
+			<P align=center><input type="checkbox" name="dostav" value="Отрисовка логотипа, графического изображения"  onClick="countdostavka(); count_price();" id="dostavka1"> <label for="dostav1">БЕСПЛАТНО</label></P>
 		
 		</td>
 	</tr>
