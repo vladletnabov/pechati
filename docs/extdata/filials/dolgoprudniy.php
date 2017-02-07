dfd5fd784abe0c134f6c29f0f6def80734845c46
vladletnabov <v.chigikov@yandex.ru>
2017-02-07 09:53:39 +0300

    добавлен Долгопрудный

---
+++ docs/extdata/filials/dolgoprudniy.php
@@ -0,0 +1,27 @@
+
+<!-- Insert to your webpage where you want to display the slider -->
+	
+    <script src="/extdata/slider/js/jquery.js"></script>
+    <script src="/extdata/slider/js/amazingslider.js"></script>
+    <link rel="stylesheet" type="text/css" href="/extdata/slider/js/amazingslider-63.css">
+    <script src="/extdata/slider/js/initslider-63.js"></script>
+	
+	
+    <div id="amazingslider-wrapper-1" style="display:block;position:relative;max-width:600px;padding-left:0px; padding-right:244px;margin:0px auto 0px;">
+        <div id="amazingslider-1" style="display:block;position:relative;margin:0 auto;">
+            <ul class="amazingslider-slides" style="display:none;">			
+			
+                <li><img src="/pics/contacts/dolgoprudniy/photo.jpeg" alt="1. Печати.РУ в Долгопрудном"  title="2. Печати.РУ в Долгопрудном" data-description="Наш офис" />
+                </li>
+                <li><img src="/pics/contacts/dolgoprudniy/scheme.png" alt="2. Печати.РУ в Долгопрудном"  title="1. Печати.РУ в Долгопрудном" data-description="Схема проезда" />
+                </li>
+            </ul>
+            <ul class="amazingslider-thumbnails" style="display:none;">
+                <li><img src="/pics/contacts/dolgoprudniy/photo-tn.jpeg" alt="1. Печати.РУ в Долгопрудном" title="1. Печати.РУ в Долгопрудном" /></li>
+                <li><img src="/pics/contacts/dolgoprudniy/scheme-tn.png" alt="2. Печати.РУ в Долгопрудном" title="2. Печати.РУ в Долгопрудном" /></li>
+                
+            </ul>
+        <div class="amazingslider-engine"><a href="http://amazingslider.com" title="Responsive jQuery Image Slideshow">Responsive jQuery Image Slideshow</a></div>
+        </div>
+    </div>
+<br><br>
